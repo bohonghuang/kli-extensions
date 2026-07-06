3f5ff23d0e5de841f4169625c80c571d56a2a1d2
Bohong Huang <bohonghuang@qq.com>
2026-07-06 22:34:50 +0800

    Load ~/.sbclrc on activation

---
+++ sbclrc/sbclrc.lisp
@@ -0,0 +1,19 @@
+;;;; Load ~/.sbclrc on activation, retract with :no-op.
+
+(defun install-sbclrc (protocol contribution context)
+  (declare (ignore protocol contribution))
+  (let ((init-file (merge-pathnames ".sbclrc" (user-homedir-pathname))))
+    (when (probe-file init-file)
+      (handler-case
+          (progn (load init-file)
+                  (notify context "Loaded ~/.sbclrc" :level :info))
+         (error (c)
+           (notify context (format nil "Failed to load ~~/.sbclrc: ~~A" c)
+                  :level :warn)))
+      :loaded)))
+
+(defextension sbclrc
+  (:provides
+   (effect load-sbclrc
+     #'install-sbclrc
+     :no-op)))
